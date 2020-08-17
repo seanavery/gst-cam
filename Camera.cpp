0bffceafdf673b4a6cca0da5847f55062b62c807
sean <seanpollock3344@gmail.com>
2020-08-17 18:03:29 -0500

    building gstreamer lanch sequence

--- Camera.cpp
+++ Camera.cpp
@@ -1,4 +1,5 @@
 #include <gst/gst.h>
+#include <gst/app/gstappsink.h>
 #include "Camera.h"
 #include <iostream>
 #include <sstream>
@@ -7,6 +8,7 @@ using namespace std;
 
 void Camera::init(int argc, char *argv[]) 
 {
+	cout << "gstreamer camera" << endl;
 	gst_init(&argc, &argv);
 };
 
@@ -16,25 +18,25 @@ void Camera::build()
 	ss << "nvarguscamerasrc wbmode=1 ! ";
 	ss << "nvvidconv flip-method=2 ! ";
 	ss << "videoconvert ! video/x-raw, format=(string)BGR ! ";
-	ss << "appsink";
-	this->pipeline = gst_parse_launch(ss.str().c_str(), NULL);
+	ss << "appsink name=sink";
+	GError* err = NULL;
+	this->launch = gst_parse_launch(ss.str().c_str(), &err);
+	if (err != NULL) 
+	{	
+		cout << err << endl;
+		g_error_free(err);
+		return;
+	}
+	cout << ss.str() << endl;
 };
 
 void Camera::start() 
 {
-	gst_element_set_state(this->pipeline, GST_STATE_PLAYING);
-	this->bus = gst_element_get_bus(pipeline);
-	this->loop();
+	GstPipeline* pipeline = GST_PIPELINE(this->launch);
+	// this->bus = gst_element_get_bus(this->launch);
+	// GstAppSink* appsinkElement = gst_bin_get_by_name(GST_BIN(
+	// gst_element_set_state(this->pipeline, GST_STATE_PLAYING);
+	// this->bus = gst_element_get_bus(this->pipeline);
+	// this->loop();
+	
 };
-
-void Camera::loop() {
-	while(true)
-	{
-		GstMessage* msg = gst_bus_pop(this->bus);
-
-		if (!msg)
-		{
-			break;
-		}
-	}
-}
