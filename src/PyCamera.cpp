9b418853e73e6568ba183bf01fb0d7716f2e3905
sean <seanpollock3344@gmail.com>
2020-09-17 21:16:00 -0500

    working pybind module

--- src/PyCamera.cpp
+++ src/PyCamera.cpp
@@ -1,12 +1,18 @@
 #include <pybind11/pybind11.h>
-#include "Camera.h"
+#include "Camera.cpp"
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(pycamera, m) 
+void init(Camera *camera) {
+	camera->init();
+}
+
+PYBIND11_MODULE(PyCamera, m) 
 {
 	py::class_<Camera>(m, "Camera")
 		.def("init", &Camera::init)
 		.def("open", &Camera::open)
 		.def("capture", &Camera::capture);
+
+	m.def("init", &init);
 }
