98553c62f0928734fd268a0a77fd68607068da85
sean <seanpollock3344@gmail.com>
2020-09-22 16:30:01 -0500

    lay the foundation

--- setup.py
+++ setup.py
@@ -1,19 +1,39 @@
-from setuptools import setup, Extension, find_packages
-module=Extension('PyCamera', sources=['static/pycamera.cpython-36m-aarch64-linux-gnu.so'])
-print("""
-        adf
-        adfa
-        adfad
-        """)
+import os
+import sys
+import subprocess
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir='./'):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+class CMakeBuild(build_ext):
+    def run(self):
+        self.build_temp = './btemp'
+        for ext in self.extensions:
+            print('here',)
+            self.build_extension(ext)
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        # required for auto-detection of auxiliary "native" libs
+        if not extdir.endswith(os.path.sep):
+            extdir += os.path.sep
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+                                                              self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+            
+        subprocess.check_call(['cmake', ext.sourcedir], cwd=self.build_temp, env=env)
+        subprocess.check_call(['make'], cwd=self.build_temp)
+
 setup(
     name='PyCamera',
-    version='0.0.4',
+    version='0.0.5',
     author='Sean Pollock',
     description='GStreamer Camera',
-    # packages=find_packages(),
-    # package_data={'PyCamera': ['pycamera.cpython-36m-aarch64-linux-gnu.so']},
-    # include_package_data=True,
-    # ext_modules=[]
-    package_data={'PyCamera': ['pycamera_cypython_aarch64.so']},
-    zip_safe=False, 
+    ext_modules=[CMakeExtension('PyCamera')],
+    cmdclass=dict(build_ext=CMakeBuild),
 )
